07478aeca623f9db7701f8f79b51f79fe9cf4f91
xijian <1585295963@qq.com>
2021-04-18 22:17:11 +0800

    first

---
+++ src/test/java/com/TrafficTicket/AppTest.java
@@ -0,0 +1,14 @@
+package com.TrafficTicket;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void test1()
+    {
+        assertTrue( true );
+    }
+}
