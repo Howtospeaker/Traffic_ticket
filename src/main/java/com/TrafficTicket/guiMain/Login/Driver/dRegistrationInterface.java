eaf84d4e8e79487f48c610e2a334a5f960e448ed
Sune0 <1029908553@qq.com>
2021-04-26 00:38:08 +0800

    111

--- src/main/java/com/TrafficTicket/guiMain/Login/Driver/dRegistrationInterface.java
+++ src/main/java/com/TrafficTicket/guiMain/Login/Driver/dRegistrationInterface.java
@@ -21,7 +21,7 @@ public class dRegistrationInterface {
         JPanel titlePanel = new JPanel();
         titlePanel.setLayout(new FlowLayout());
         titlePanel.add(new JLabel("账号注册"));
-        c.add(titlePanel,"North");
+        c.add(titlePanel, "North");
         //输入信息部分
         JPanel filedPanel = new JPanel();
         filedPanel.setLayout(null);
@@ -33,14 +33,14 @@ public class dRegistrationInterface {
         JLabel jAge = new JLabel("年龄");
         JLabel jPassword = new JLabel("密码");
         JLabel jPasswordAgain = new JLabel("再次输入密码");
-        jUsename.setBounds(30,20,50,20);
-        jName.setBounds(30,60,50,20);
-        jSex.setBounds(30,100,50,20);
-        jIdNum.setBounds(30,140,50,20);
-        jLicenseNum.setBounds(30,180,80,20);
-        jAge.setBounds(30,220,80,20);
-        jPassword.setBounds(30,260,80,20);
-        jPasswordAgain.setBounds(30,300,80,20);
+        jUsename.setBounds(30, 20, 50, 20);
+        jName.setBounds(30, 60, 50, 20);
+        jSex.setBounds(30, 100, 50, 20);
+        jIdNum.setBounds(30, 140, 50, 20);
+        jLicenseNum.setBounds(30, 180, 80, 20);
+        jAge.setBounds(30, 220, 80, 20);
+        jPassword.setBounds(30, 260, 80, 20);
+        jPasswordAgain.setBounds(30, 300, 80, 20);
         filedPanel.add(jUsename);
         filedPanel.add(jName);
         filedPanel.add(jSex);
@@ -53,42 +53,44 @@ public class dRegistrationInterface {
 
         JTextField usename = new JTextField();
         JTextField name = new JTextField();
-        JPanel JCom = new JPanel();
-        JComboBox<String> sex = new JComboBox<String>();
-        sex.addItem("男");
-        sex.addItem("女");
-        sex.addItemListener(new ItemListener() {
-            @Override
-            public void itemStateChanged(ItemEvent itemEvent) {
-                if (itemEvent.getStateChange() == ItemEvent.SELECTED) {
-                    //这里写你的任务 ，比如取到现在的值
-                    String sexText =(String) sex.getSelectedItem();
-                }
-            }
-        });
+
+        JRadioButton male = new JRadioButton("男", true);
+        JRadioButton female = new JRadioButton("女", false);
+        male.setBounds(110, 100, 140, 20);
+        male.setBounds(140, 100, 140, 20);
+
+        //实现单选效果
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(male);
+        bg.add(female);
+        String sexText = bg.isSelected(male.getModel()) ? male.getText() : female.getText();
+
+
         JTextField idNum = new JTextField();
         JTextField licenseNum = new JTextField();
         JTextField age = new JTextField();
         JTextField password = new JTextField();
         JTextField passwordAgain = new JTextField();
 
-        usename.setBounds(110,20,140,20);
-        name.setBounds(110,60,140,20);
-        sex.setBounds(110,100,140,20);
-        idNum.setBounds(110,140,140,20);
-        licenseNum.setBounds(110,180,140,20);
-        age.setBounds(110,220,140,20);
-        password.setBounds(110,260,140,20);
-        passwordAgain.setBounds(110,300,140,20);
+        usename.setBounds(110, 20, 140, 20);
+        name.setBounds(110, 60, 140, 20);
+        male.setBounds(110, 100, 140, 20);
+        male.setBounds(140, 100, 140, 20);
+        idNum.setBounds(110, 140, 140, 20);
+        licenseNum.setBounds(110, 180, 140, 20);
+        age.setBounds(110, 220, 140, 20);
+        password.setBounds(110, 260, 140, 20);
+        passwordAgain.setBounds(110, 300, 140, 20);
         filedPanel.add(usename);
         filedPanel.add(name);
-        filedPanel.add(sex);
+        filedPanel.add(male);
+        filedPanel.add(female);
         filedPanel.add(idNum);
         filedPanel.add(licenseNum);
         filedPanel.add(age);
         filedPanel.add(password);
         filedPanel.add(passwordAgain);
-        c.add(filedPanel,"Center");
+        c.add(filedPanel, "Center");
         //按钮设置
         JPanel buttonPanel = new JPanel();
 
@@ -107,9 +109,9 @@ public class dRegistrationInterface {
                 String ageText = age.getText().trim();
                 String passwordText = password.getText().trim();
 
-                Driver driver = new Driver(Integer.parseInt(idNumText),nameText,sexText,Integer.parseInt(ageText),Integer.valueOf(licenseNumText),usenameText,passwordText);
+                Driver driver = new Driver(Integer.parseInt(idNumText), nameText, sexText, Integer.parseInt(ageText), Integer.valueOf(licenseNumText), usenameText, passwordText);
 
-                if(driverController.register(driver)){
+                if (driverController.register(driver)) {
                     new dLoginInterface().init();
                     jf2.dispose();
                 }
@@ -126,7 +128,7 @@ public class dRegistrationInterface {
             }
         });
 
-        c.add(buttonPanel,"South");
+        c.add(buttonPanel, "South");
         jf2.setResizable(false);
         jf2.setVisible(true);
     }
